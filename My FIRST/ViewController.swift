40c1e5fc39b8fc74b957497fb9e92c98349c9dcb
t-11 <mcz.rekaashiamo@gmail.com>
2021-01-15 23:00:10 +0900

    commit

--- My FIRST/ViewController.swift
+++ My FIRST/ViewController.swift
@@ -12,8 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // (1)ラベルに文字を入れる
+        outputLabel.text = "Hello Swift!"
     }
 
 
+    @IBOutlet weak var outputLabel: UILabel!
 }
 
